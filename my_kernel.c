0a1e011281f7afb3ae252a9bcb50a78e7be51cb5
hfs <hanfs390@foxmail.com>
2019-07-29 20:32:12 -0700

    [user] recv and send by diff function
    Signed-off-by: hfs <hanfs390@foxmail.com>

--- my_kernel.c
+++ my_kernel.c
@@ -9,79 +9,77 @@
 
 struct sock *nl_sk = NULL;
 EXPORT_SYMBOL_GPL(nl_sk);
+u32 pid;
 
-void send_to_user(char *str, u32 pid)
+void send_to_user(char *str, int str_len)
 {
-  struct sk_buff *skb;
-  struct nlmsghdr *nlh;
-  int rc;
-  int len = NLMSG_SPACE(1200);
-    skb = alloc_skb(len, GFP_ATOMIC);
-    if (!skb){
-      printk(KERN_ERR "net_link: allocate failed.\n");
-      return;
-    }
-    nlh = nlmsg_put(skb,0,0,0,1200,0);
-    NETLINK_CB(skb).portid = 0; 
+	struct sk_buff *skb;
+	struct nlmsghdr *nlh;
+	int rc;
+  	int len = NLMSG_SPACE(1200);
+    	skb = alloc_skb(len, GFP_ATOMIC);
+    	if (!skb) {
+      		printk(KERN_ERR "net_link: allocate failed.\n");
+      		return;
+    	}
+    	nlh = nlmsg_put(skb,0,0,0,1200,0);
+    	NETLINK_CB(skb).portid = 0; 
 
-    memcpy(NLMSG_DATA(nlh), str, 15);
-    printk("net_link: going to send.\n");
-    rc = netlink_unicast(nl_sk, skb, pid, MSG_DONTWAIT);
-    if (rc < 0) {
-      printk(KERN_ERR "net_link: can not unicast skb (%d)\n", rc);
-    }
-
-
+    	memcpy(NLMSG_DATA(nlh), str, str_len);
+    	printk("net_link: going to send.\n");
+    	rc = netlink_unicast(nl_sk, skb, pid, MSG_DONTWAIT);
+    	if (rc < 0) {
+      		printk(KERN_ERR "net_link: can not unicast skb (%d)\n", rc);
+    	}
 }
 void nl_data_ready (struct sk_buff *__skb)
 {
-  struct sk_buff *skb;
-  struct nlmsghdr *nlh;
-  u32 pid;
-  char str[100];
+  	struct sk_buff *skb;
+  	struct nlmsghdr *nlh;
+  	char str[100];
 
-  printk("net_link: data is ready to read.\n");
-  skb = skb_get(__skb);
+  	printk("net_link: data is ready to read.\n");
+  	skb = skb_get(__skb);
 
-  if (skb->len >= NLMSG_SPACE(0)) {
-    nlh = nlmsg_hdr(skb);
-    printk("net_link: recv %s.\n", (char *)NLMSG_DATA(nlh));
-    memcpy(str,NLMSG_DATA(nlh), sizeof(str)); 
-    pid = nlh->nlmsg_pid; 
-    printk("net_link: pid is %d\n", pid);
-    kfree_skb(skb);
-	send_to_user("hello1", pid);
-    printk("net_link: send is ok.\n");
-  }
-  return;
+  	if (skb->len >= NLMSG_SPACE(0)) {
+    		nlh = nlmsg_hdr(skb);
+    		printk("net_link: recv %s.\n", (char *)NLMSG_DATA(nlh));
+    		memcpy(str,NLMSG_DATA(nlh), sizeof(str)); 
+    		pid = nlh->nlmsg_pid; 
+    		printk("net_link: pid is %d\n", pid);
+    		kfree_skb(skb);
+		send_to_user("shenmeazhshiu", 15);
+    		printk("net_link: send is ok.\n");
+  	}
+  	return;
 }
 
 static int test_netlink(void) {
 
-  struct netlink_kernel_cfg cfg = {
-    .input = nl_data_ready,
-  };
-  nl_sk = netlink_kernel_create(&init_net, NETLINK_TEST, &cfg);
+	struct netlink_kernel_cfg cfg = {
+    		.input = nl_data_ready,
+  	};
+  	nl_sk = netlink_kernel_create(&init_net, NETLINK_TEST, &cfg);
 
-  if (!nl_sk) {
-    printk(KERN_ERR "net_link: Cannot create netlink socket.\n");
-    return -EIO;
-  }
-  printk("net_link: create socket ok.\n");
-  return 0;
+  	if (!nl_sk) {
+    		printk(KERN_ERR "net_link: Cannot create netlink socket.\n");
+    		return -EIO;
+  	}
+  	printk("net_link: create socket ok.\n");
+	return 0;
 }
 
 static int __init init_kernel(void)
 {
-  test_netlink();
-  return 0;
+  	test_netlink();
+  	return 0;
 }
 static void __exit cleanup_kernel(void)
 {
-  if (nl_sk != NULL){
-    sock_release(nl_sk->sk_socket);
-  }
-  printk("net_link: remove ok.\n");
+  	if (nl_sk != NULL) {
+    		sock_release(nl_sk->sk_socket);
+  	}
+  	printk("net_link: remove ok.\n");
 }
 MODULE_LICENSE("GPL");
 module_init(init_kernel);
--- user.c
+++ user.c
@@ -10,14 +10,38 @@
 #include <linux/socket.h>
 
 #define MAX_PAYLOAD 1024 
-struct sockaddr_nl src_addr, dest_addr;
-struct nlmsghdr *nlh = NULL;
-struct iovec iov;
-int sock_fd;
-struct msghdr msg;
+void receive_msg(int sock_fd)
+{
+	struct sockaddr_nl dest_addr;
+	struct nlmsghdr *nlh = NULL;
+	struct iovec iov;
+	struct msghdr msg;
+	int ret = 0;
+        memset(&dest_addr, 0, sizeof(dest_addr));
+        dest_addr.nl_family = AF_NETLINK;
+        dest_addr.nl_pid = 0; 
+        dest_addr.nl_groups = 0; 
+        nlh=(struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
+        iov.iov_base = (void *)nlh;
+        iov.iov_len = 120;
+        msg.msg_name = (void *)&dest_addr;
+        msg.msg_namelen = sizeof(dest_addr);
+        msg.msg_iov = &iov;
+        msg.msg_iovlen = 1;
+        memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
+        printf(" Waiting message. ...\n");
+        ret = recvmsg(sock_fd, &msg, 0);
+	printf(" msg len %d\n", ret);
+        printf(" Received message payload: %s\n", (char *)NLMSG_DATA(nlh));
 
+}
 int main(int argc, char* argv[])
 {
+	struct sockaddr_nl src_addr, dest_addr;
+	struct nlmsghdr *nlh = NULL;
+	struct iovec iov;
+	int sock_fd;
+	struct msghdr msg;
         sock_fd = socket(PF_NETLINK, SOCK_RAW, 21);
         memset(&msg, 0, sizeof(msg));
         memset(&src_addr, 0, sizeof(src_addr));
@@ -25,18 +49,18 @@ int main(int argc, char* argv[])
         src_addr.nl_pid = getpid(); 
         src_addr.nl_groups = 0; 
         bind(sock_fd, (struct sockaddr*)&src_addr, sizeof(src_addr));
+
         memset(&dest_addr, 0, sizeof(dest_addr));
         dest_addr.nl_family = AF_NETLINK;
         dest_addr.nl_pid = 0; 
         dest_addr.nl_groups = 0; 
-
         nlh=(struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
         
         nlh->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
         nlh->nlmsg_pid = getpid(); 
         nlh->nlmsg_flags = 0;
         
-        strcpy(NLMSG_DATA(nlh), "Hello you!");
+        strcpy(NLMSG_DATA(nlh), "Hello you! adf");
 
         iov.iov_base = (void *)nlh;
         iov.iov_len = nlh->nlmsg_len;
@@ -49,11 +73,7 @@ int main(int argc, char* argv[])
         sendmsg(sock_fd, &msg, 0);
 
         
-        memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
-        printf(" Waiting message. ...\n");
-        recvmsg(sock_fd, &msg, 0);
-        printf(" Received message payload: %s\n", (char *)NLMSG_DATA(nlh));
-
+	receive_msg(sock_fd);
          
         close(sock_fd);
 }
